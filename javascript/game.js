513913ee72a63db90e402bb6b9164e3c79a20986
Jesse Ajioh <ajiohjesse@gmail.com>
2023-01-13 20:18:13 +0000

    update readme

--- javascript/game.js
+++ javascript/game.js
@@ -1,130 +1,128 @@
-import { randomizeArray, range, sampleOne } from './utils.js'
+import { randomizeArray, range, sampleOne } from "./utils.js";
 import {
   correctCardMessages,
   firstCardMessages,
   gameLostMessages,
   wrongCardMessages,
-} from './lib.js'
+} from "./lib.js";
 
 //*****************************************
 // INTERFACE
 //*****************************************
 
 // sound toggle button
-const soundBtn = document.getElementById('sound-btn')
+const soundBtn = document.getElementById("sound-btn");
 
 //dashboard
-const screen = document.getElementById('game-message')
-const completedEl = document.getElementById('completed')
-const sneaksEl = document.getElementById('sneaks')
-const attemptsEl = document.getElementById('attempts')
+const screen = document.getElementById("game-message");
+const completedEl = document.getElementById("completed");
+const sneaksEl = document.getElementById("sneaks");
+const attemptsEl = document.getElementById("attempts");
 
 //buttons
-const resetBtn = document.getElementById('game-reset')
-const sneakBtn = document.getElementById('game-sneak')
-const showBtn = document.getElementById('game-show')
+const resetBtn = document.getElementById("game-reset");
+const sneakBtn = document.getElementById("game-sneak");
+const showBtn = document.getElementById("game-show");
 
 //cards
-const cardsEl = document.getElementById('game-cards')
-
-
+const cardsEl = document.getElementById("game-cards");
 
 //*****************************************
 // SOUND EFFECTS
 //*****************************************
-const correctCardSND = new Audio('../assets/audio/correct-card.mp3')
-const wrongCardSND = new Audio('../assets/audio/wrong-card.mp3')
-const firstCardSND = new Audio('../assets/audio/first-card.mp3')
-const gameLostSND = new Audio('../assets/audio/game-lost.mp3')
-const gamePlayingSND = new Audio('../assets/audio/game-playing.mp3')
-const sneakSND = new Audio('../assets/audio/peak.mp3')
-const showCardSND = new Audio('../assets/audio/show-cards.mp3')
-const winSND = new Audio('../assets/audio/win.mp3')
+const correctCardSND = new Audio("../assets/audio/correct-card.mp3");
+const wrongCardSND = new Audio("../assets/audio/wrong-card.mp3");
+const firstCardSND = new Audio("../assets/audio/first-card.mp3");
+const gameLostSND = new Audio("../assets/audio/game-lost.mp3");
+const gamePlayingSND = new Audio("../assets/audio/game-playing.mp3");
+const sneakSND = new Audio("../assets/audio/peak.mp3");
+const showCardSND = new Audio("../assets/audio/show-cards.mp3");
+const winSND = new Audio("../assets/audio/win.mp3");
 
-gamePlayingSND.volume = 1
-correctCardSND.volume = 0.25
-wrongCardSND.volume = 0.1
-firstCardSND.volume = 0.25
-gameLostSND.volume = 0.2
-sneakSND.volume = 0.25
-showCardSND.volume = 0.2
-winSND.volume = 0.2
+gamePlayingSND.volume = 1;
+correctCardSND.volume = 0.25;
+wrongCardSND.volume = 0.1;
+firstCardSND.volume = 0.25;
+gameLostSND.volume = 0.2;
+sneakSND.volume = 0.25;
+showCardSND.volume = 0.2;
+winSND.volume = 0.2;
 
 //*****************************************
 // EVENT LISTENERS
 //*****************************************
-document.addEventListener('DOMContentLoaded', resetGame)
-resetBtn.addEventListener('click', resetGame)
-sneakBtn.addEventListener('click', sneak)
-showBtn.addEventListener('click', endGame)
-soundBtn.addEventListener('click', toggleSound)
+document.addEventListener("DOMContentLoaded", resetGame);
+resetBtn.addEventListener("click", resetGame);
+sneakBtn.addEventListener("click", sneak);
+showBtn.addEventListener("click", endGame);
+soundBtn.addEventListener("click", toggleSound);
 
 //*****************************************
 // VARIABLES
 //*****************************************
-const DEFAULT_SNEAKS = 2
-const DEFAULT_ATTEMPTS = 15
+const DEFAULT_SNEAKS = 2;
+const DEFAULT_ATTEMPTS = 15;
 
-let gameLost = false
-let isPlaying = false
-let clickFirstCard = false
+let gameLost = false;
+let isPlaying = false;
+let clickFirstCard = false;
 
-let openedCards = []
-let sneaks = DEFAULT_SNEAKS
-let attempts = DEFAULT_ATTEMPTS
+let openedCards = [];
+let sneaks = DEFAULT_SNEAKS;
+let attempts = DEFAULT_ATTEMPTS;
 
-let firstCard = null
-let firstCardIndex = null
+let firstCard = null;
+let firstCardIndex = null;
 
-let secondCard = null
-let secondCardIndex = null
+let secondCard = null;
+let secondCardIndex = null;
 
 // Id for setTimout function on game screen
-let screenTimeout = null
+let screenTimeout = null;
 
-let sneakTime = 2000
-let cardsAreOpen = false
+let sneakTime = 2000;
+let cardsAreOpen = false;
 
 //get sound preference from local storage
-let shouldPlaySound = getShouldPlaySound()
+let shouldPlaySound = getShouldPlaySound();
 
 //*****************************************
 // FUNCTIONS
 //*****************************************
 
 function renderGame(e) {
-  if (gameLost) return
+  if (gameLost) return;
 
-  const target = e.target
-  const targetId = target.dataset.id
-  const targetIndex = target.dataset.index
+  const target = e.target;
+  const targetId = target.dataset.id;
+  const targetIndex = target.dataset.index;
 
   // prevent game-cards wrapper from triggering game
-  if (target.id === 'game-cards') return
+  if (target.id === "game-cards") return;
 
-  isPlaying = true
-  playGameMusic()
+  isPlaying = true;
+  playGameMusic();
 
   if (!targetId) {
-    return brieflyRenderScreen('Card is already open.')
+    return brieflyRenderScreen("Card is already open.");
   }
 
   //first selection
   if (!clickFirstCard) {
-    clickFirstCard = true
-    firstCard = targetId
-    firstCardIndex = targetIndex
+    clickFirstCard = true;
+    firstCard = targetId;
+    firstCardIndex = targetIndex;
 
-    playSound(firstCardSND)
-    showCard(target)
-    renderScreen(sampleOne(firstCardMessages))
+    playSound(firstCardSND);
+    showCard(target);
+    renderScreen(sampleOne(firstCardMessages));
 
-    return
+    return;
   }
 
   //second selection
-  secondCard = targetId
-  secondCardIndex = targetIndex
+  secondCard = targetId;
+  secondCardIndex = targetIndex;
 
   /**
    * check if;
@@ -134,91 +132,91 @@ function renderGame(e) {
    */
 
   if (secondCardIndex === firstCardIndex) {
-    return brieflyRenderScreen('Card is already open.')
+    return brieflyRenderScreen("Card is already open.");
   }
 
   // second selection doesn't match first
   if (firstCard !== secondCard) {
-    brieflyShowCard(target)
-    renderScreen(sampleOne(wrongCardMessages))
+    brieflyShowCard(target);
+    renderScreen(sampleOne(wrongCardMessages));
 
-    playSound(wrongCardSND)
-    reduceAttempts()
-    renderDashBoard()
+    playSound(wrongCardSND);
+    reduceAttempts();
+    renderDashBoard();
 
-    return
+    return;
   }
 
   // second selection matches first
-  showCard(target)
-  renderScreen(sampleOne(correctCardMessages))
-  playSound(correctCardSND)
+  showCard(target);
+  renderScreen(sampleOne(correctCardMessages));
+  playSound(correctCardSND);
 
-  openedCards.push(firstCard)
-  clickFirstCard = false
+  openedCards.push(firstCard);
+  clickFirstCard = false;
 
-  renderDashBoard()
+  renderDashBoard();
 
   //game won if all cards are opened
-  if (openedCards.length === 8) gameWon()
+  if (openedCards.length === 8) gameWon();
 }
 
 function resetGame() {
-  gameLost = false
-  isPlaying = false
-  cardsAreOpen = false
+  gameLost = false;
+  isPlaying = false;
+  cardsAreOpen = false;
 
-  renderCards()
-  resetScreen()
-  resetDashboard()
-  addCardEvent()
-  enableSneakBtn()
-  hideAllCards()
-  stopGameMusic()
+  renderCards();
+  resetScreen();
+  resetDashboard();
+  addCardEvent();
+  enableSneakBtn();
+  hideAllCards();
+  stopGameMusic();
 }
 
 function playGameMusic() {
   if (shouldPlaySound) {
-    gamePlayingSND.loop = true
-    gamePlayingSND.play()
+    gamePlayingSND.loop = true;
+    gamePlayingSND.play();
   }
 }
 
 function stopGameMusic() {
-  gamePlayingSND.pause()
-  gamePlayingSND.currentTime = 0
+  gamePlayingSND.pause();
+  gamePlayingSND.currentTime = 0;
 }
 
 function playSound(sound) {
   if (shouldPlaySound) {
-    sound.currentTime = 0
-    sound.play()
+    sound.currentTime = 0;
+    sound.play();
   }
 }
 
 function toggleSound() {
-  shouldPlaySound = !shouldPlaySound
-  localStorage.setItem('game-sound', shouldPlaySound)
-  stopGameMusic()
+  shouldPlaySound = !shouldPlaySound;
+  localStorage.setItem("game-sound", shouldPlaySound);
+  stopGameMusic();
 
   if (isPlaying) {
-    playGameMusic()
+    playGameMusic();
   }
 }
 
 function getShouldPlaySound() {
-  const userPreference = localStorage.getItem('game-sound')
+  const userPreference = localStorage.getItem("game-sound");
 
   if (userPreference === null) {
-    return true
+    return true;
   }
 
-  return JSON.parse(userPreference)
+  return JSON.parse(userPreference);
 }
 
 function renderCards() {
   //array of numbers from 1-8
-  const iconList = range(1, 9)
+  const iconList = range(1, 9);
 
   /** randomize icon list on each render,
    *  duplicate each array item to get icon pairs.
@@ -229,186 +227,184 @@ function renderCards() {
 <div class="game-card-image">
 <img src="./assets/icons/${iconNumber}.svg" alt="${iconNumber}" />
 </div>
-</div>`
+</div>`;
     })
-    .join(' ')
+    .join(" ");
 
-  cardsEl.innerHTML = HTMLString
+  cardsEl.innerHTML = HTMLString;
 }
 
 function resetDashboard() {
-  openedCards = []
-  sneaks = DEFAULT_SNEAKS
-  attempts = DEFAULT_ATTEMPTS
-  clickFirstCard = false
+  openedCards = [];
+  sneaks = DEFAULT_SNEAKS;
+  attempts = DEFAULT_ATTEMPTS;
+  clickFirstCard = false;
 
-  renderDashBoard()
+  renderDashBoard();
 }
 
 function renderDashBoard() {
-  completedEl.textContent = openedCards.length
-  sneaksEl.textContent = sneaks
-  attemptsEl.textContent = attempts
+  completedEl.textContent = openedCards.length;
+  sneaksEl.textContent = sneaks;
+  attemptsEl.textContent = attempts;
 }
 
 function showCard(target) {
-  target.classList.add('open')
+  target.classList.add("open");
 }
 
 function showAllCards() {
-  cardsEl.classList.add('open')
+  cardsEl.classList.add("open");
 }
 
 function hideCard(target) {
-  target.classList.remove('open')
+  target.classList.remove("open");
 }
 
 function hideAllCards() {
-  cardsEl.classList.remove('open')
+  cardsEl.classList.remove("open");
 }
 
 function brieflyShowCard(target) {
-  showCard(target)
+  showCard(target);
 
   setTimeout(() => {
-    hideCard(target)
-  }, 800)
+    hideCard(target);
+  }, 800);
 }
 
 function brieflyShowAllCards() {
-  showAllCards()
+  showAllCards();
 
   setTimeout(() => {
-    hideAllCards()
-  }, sneakTime)
+    hideAllCards();
+  }, sneakTime);
 }
 
 function resetScreen() {
   screen.textContent = `Select a card and guess the 
   position of it's matching pair. Click on any card to 
-  start the game!`
+  start the game!`;
 }
 
 function renderScreen(message) {
   // if timeout is already running, cancel it.
-  if (screenTimeout) clearTimeout(screenTimeout)
+  if (screenTimeout) clearTimeout(screenTimeout);
 
-  screen.textContent = message
+  screen.textContent = message;
 }
 
 function brieflyRenderScreen(message) {
-  screen.textContent = message
+  screen.textContent = message;
 
   // if timeout is already running, cancel it.
-  if (screenTimeout) clearTimeout(screenTimeout)
+  if (screenTimeout) clearTimeout(screenTimeout);
 
   screenTimeout = setTimeout(() => {
-    screen.textContent = ''
-  }, 1200)
+    screen.textContent = "";
+  }, 1200);
 }
 
 function reduceAttempts() {
-  attempts--
+  attempts--;
   if (attempts <= 0) {
-    attempts = 0
+    attempts = 0;
 
-    gameFailed()
+    gameFailed();
   }
 }
 
 function addCardEvent() {
-  cardsEl.addEventListener('click', renderGame)
+  cardsEl.addEventListener("click", renderGame);
 }
 
 function removeCardEvent() {
-  cardsEl.removeEventListener('click', renderGame)
+  cardsEl.removeEventListener("click", renderGame);
 }
 
 function breiflyRemoveCardEvent() {
- removeCardEvent()
+  removeCardEvent();
 
   setTimeout(() => {
-    addCardEvent()
-  }, sneakTime)
+    addCardEvent();
+  }, sneakTime);
 }
 
-
-
 function disableSneakBtn() {
-  sneakBtn.setAttribute('disabled', 'true')
+  sneakBtn.setAttribute("disabled", "true");
 }
 
 function enableSneakBtn() {
-  sneakBtn.removeAttribute('disabled')
+  sneakBtn.removeAttribute("disabled");
 }
 
 function brieflyDisableSneakBtn() {
-  disableSneakBtn()
+  disableSneakBtn();
 
   setTimeout(() => {
-    enableSneakBtn()
-  }, sneakTime)
+    enableSneakBtn();
+  }, sneakTime);
 }
 
 function brieflydisableShowBtn() {
-  showBtn.setAttribute('disabled', 'true')
+  showBtn.setAttribute("disabled", "true");
 
   setTimeout(() => {
-    showBtn.removeAttribute('disabled')
-  }, sneakTime)
+    showBtn.removeAttribute("disabled");
+  }, sneakTime);
 }
 
 function sneak() {
-  if (!isPlaying) return
- 
-  sneaks--
+  if (!isPlaying) return;
+
+  sneaks--;
 
   if (sneaks >= 0) {
-    renderDashBoard()
-    brieflyShowAllCards()
-    brieflyDisableSneakBtn()
-    brieflydisableShowBtn()
-    breiflyRemoveCardEvent()
-    playSound(sneakSND)
-    return
+    renderDashBoard();
+    brieflyShowAllCards();
+    brieflyDisableSneakBtn();
+    brieflydisableShowBtn();
+    breiflyRemoveCardEvent();
+    playSound(sneakSND);
+    return;
   }
 
-  sneaks = 0
-  renderDashBoard()
-  brieflyRenderScreen('You have no sneaks left.')
+  sneaks = 0;
+  renderDashBoard();
+  brieflyRenderScreen("You have no sneaks left.");
 }
 
 function endGame() {
-  showAllCards()
-  removeCardEvent()
-  disableSneakBtn()
-  renderScreen('Thanks for Playing. Reset the game to play again.')
-  stopGameMusic()
+  showAllCards();
+  removeCardEvent();
+  disableSneakBtn();
+  renderScreen("Thanks for Playing. Reset the game to play again.");
+  stopGameMusic();
 
-  if (!cardsAreOpen) playSound(showCardSND)
-  cardsAreOpen = true
-  isPlaying = false
+  if (!cardsAreOpen) playSound(showCardSND);
+  cardsAreOpen = true;
+  isPlaying = false;
 }
 
 function gameFailed() {
-  gameLost = true
-  isPlaying = false
-  cardsAreOpen = true
+  gameLost = true;
+  isPlaying = false;
+  cardsAreOpen = true;
 
-  renderScreen(sampleOne(gameLostMessages))
-  showAllCards()
-  removeCardEvent()
-  disableSneakBtn()
-  stopGameMusic()
-  playSound(gameLostSND)
+  renderScreen(sampleOne(gameLostMessages));
+  showAllCards();
+  removeCardEvent();
+  disableSneakBtn();
+  stopGameMusic();
+  playSound(gameLostSND);
 }
 
 function gameWon() {
-  cardsAreOpen = true
+  cardsAreOpen = true;
 
-  stopGameMusic()
-  playSound(winSND)
-  renderScreen('Congrats! Game won. You did a nice job.')
-  disableSneakBtn()
-  removeCardEvent()
+  stopGameMusic();
+  playSound(winSND);
+  renderScreen("Congrats! Game won. You did a nice job.");
+  disableSneakBtn();
+  removeCardEvent();
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Guess A Card
+
+[Visit Webpage](https://guess-a-card-v2.vercel.app)
+
+Guess A Card is a simple browser game built with vanilla javascript, html and css. The first version of this game was built during my introductory phase to javascript. This repository is the second version of the game which features cleaner code and a bit more functionality.
+
+Browse [this repository](https://github.com/ajiohjesse/guess-a-card-V1) for the first version.
